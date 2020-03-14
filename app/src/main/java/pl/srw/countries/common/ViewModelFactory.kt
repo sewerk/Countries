efcbaee4c558f687c08dbb73ce8bc2a52b6be316
Kamil Seweryn <sewerk@gmail.com>
2020-03-14 16:03:22 +0100

    Fix view model leaking

--- app/src/main/java/pl/srw/countries/common/ViewModelFactory.kt
+++ app/src/main/java/pl/srw/countries/common/ViewModelFactory.kt
@@ -3,13 +3,14 @@ package pl.srw.countries.common
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
 class ViewModelFactory<T : ViewModel> @Inject constructor(
-    private val viewModel: T
+    private val viewModel: Provider<T>
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>) = viewModel as T
+    override fun <T : ViewModel> create(modelClass: Class<T>) = viewModel.get() as T
 }
